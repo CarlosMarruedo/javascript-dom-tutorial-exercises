6abc89c897f9c13c4bd3cf7266db4dcb86a7b938
CarlosMarruedo <carlosmarruedo@gmail.com>
2021-03-26 10:27:20 +0000

    Some exercise done

--- exercises/05-Create-DOM-Element-First/index.js
+++ exercises/05-Create-DOM-Element-First/index.js
@@ -4,3 +4,7 @@ document.querySelector("#myDiv").appendChild(elem);
 
 //Your code here
 // Don'change the code above
+var element = document.createElement("P");
+element.innerHTML = "Hello World";
+element.style.backgroundColor = "yellow";
+document.querySelector("#myDiv").appendChild(element);
--- exercises/09-Remove-DOM-Element-part-two/index.js
+++ exercises/09-Remove-DOM-Element-part-two/index.js
@@ -1,1 +1,3 @@
-
+let list = document.querySelector("#parentLI");
+let childs = list.childNodes;
+childs[3].removeChild();
